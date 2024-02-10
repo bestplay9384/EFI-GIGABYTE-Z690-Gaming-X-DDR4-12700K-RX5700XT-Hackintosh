3d942d2f1a4821ec85fd721486f6668bc9f88d9a
bestplay9384 <bestplay9@me.com>
2024-02-10 16:49:42 +0100

    Mac OS Sonoma 14.3.1 update && fixed typo in RestrictEvents kext NVRAM flag revpatch => now incremental OS updates should work fine after reverting OCLP root patches (if were enabled)

--- _/scripts/remove.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR_PREFIX=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-
-plutil -replace PlatformInfo.Generic.MLB -string "[REPLACE]" ${DIR_PREFIX}/../../EFI/OC/config.plist
-plutil -replace PlatformInfo.Generic.ROM -string "[REPLACE]" ${DIR_PREFIX}/../../EFI/OC/config.plist
-plutil -replace PlatformInfo.Generic.SystemSerialNumber -string "[REPLACE]" ${DIR_PREFIX}/../../EFI/OC/config.plist
-plutil -replace PlatformInfo.Generic.SystemUUID -string "[REPLACE]" ${DIR_PREFIX}/../../EFI/OC/config.plist
